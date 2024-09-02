e0f3dc580224162794d6ac13a6c8b907b4e61488
Simon Emmanuel <simonemmanuel743@gmail.com>
2024-09-02 15:36:07 +0100

    connect mongoDb and create model for user and note

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const mongoose = require("mongoose");
+
 const corsOptions = require("./config/corsOption");
 const errorHandler = require("./middleware/errorHandler");
-const { logger } = require("./middleware/logger");
+const { logger, logEvents } = require("./middleware/logger");
+const connectDB = require("./config/dbConn");
 const PORT = process.env.PORT || 3500;
+
+console.log(process.env.NODE_ENV);
+
+connectDB();
 
 app.use(logger);
 
@@ -35,4 +43,15 @@ app.all("*", (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+mongoose.connections.once("open", () => {
+  console.log("Connected to MongoDb");
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+  logEvents(
+    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    "mongoErrLog.log"
+  );
+});
