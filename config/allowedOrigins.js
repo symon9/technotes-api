abb0f2896aedee3128b7c43a700d18bbd7772f5b
Simon Emmanuel <simonemmanuel743@gmail.com>
2024-09-18 19:11:12 +0100

    updated allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
   // "http://localhost:3000",
-  "https://technotes-urnj.onrender.com/",
+  "https://technotes-api-53o7.onrender.com",
 ];
 
 module.exports = allowedOrigins;
